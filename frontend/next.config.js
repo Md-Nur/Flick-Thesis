2a6b8b0d9c202921337e1916cfd280abcfaa41c9
Nur <md.nurealamsiddiquee2004@gmail.com>
2022-02-20 14:22:19 +0600

    complete full frontend

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,3 +4,17 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+
+// // next.config.js
+
+// const withImages = require("next-images");
+
+// module.exports = withImages({
+//     images: {
+//         domains: ['localhost:3001']
+//     },
+//     webpack(config, options) {
+//         return config;
+//     }
+// });
